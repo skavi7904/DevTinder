a78ed94262a9e3cda19fe8a7e76d2903d7fb2b80
skavi7904 <skavi7904@gmail.com>
2025-07-20 00:54:12 +0530

    ref,populate,String(objId)

--- src/router/user.js
+++ src/router/user.js
@@ -3,12 +3,14 @@ const { userAuth } = require("../middlewares/auth");
 const { ConnectionRequestModel } = require("../models/connectionRequest");
 const userRouter = express.Router();
 
+const showableFields = ['firstName','lastName','email','age','bio','skills','gender','photoUrl']
+
 userRouter.get("/received/requests", userAuth, async(req, res )=> {
     try {
         const receivedRequests = await ConnectionRequestModel.find({
             toUserId: req._id,
             status: 'interested'
-        })
+        }).populate("fromUserId",showableFields)
         res.json({receivedRequests});
     } catch(err) {
         res.status(400).json({error: err?.message});
@@ -20,8 +22,12 @@ userRouter.get("/user/connections", userAuth, async(req, res)=> {
         const connections = await ConnectionRequestModel.find({
             $or: [{ fromUserId: req._id},{toUserId: req._id}],
           status: "accepted",
-        });
-        res.json({ connections });
+        }).populate("fromUserId", showableFields.join(" ")).populate("toUserId", showableFields.join(" "));
+        const correctConnectionResults = connections.map((connection) => {
+            if (String(connection.fromUserId._id)===(req._id)) return connection.toUserId;
+            return connection.fromUserId;
+        })
+        res.json({ connections: correctConnectionResults });
     } catch(err) {
         res.status(400).json({error: err?.message});
     }
